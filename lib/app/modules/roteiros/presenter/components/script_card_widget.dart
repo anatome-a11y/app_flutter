94342413fe7cf42205a27c83369d3e2fb21b646a
André Neves <andreneves3@gmail.com>
2021-03-21 21:00:31 -0300

    fix: enable back button in script home page

--- lib/app/modules/roteiros/presenter/components/script_card_widget.dart
+++ lib/app/modules/roteiros/presenter/components/script_card_widget.dart
@@ -11,7 +11,7 @@ class ScriptCardWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Modular.to.navigate('/scripts');
+        Modular.to.pushNamed('/scripts');
       },
       child: Card(
         clipBehavior: Clip.antiAlias,
