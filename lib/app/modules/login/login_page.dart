d38b231ff98062838afe67fb18d920458bbee2a9
André Neves <andreneves3@gmail.com>
2021-03-29 00:41:27 -0300

    feat: adjust login page

--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -9,6 +10,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final formKey = new GlobalKey<FormState>();
 
+  bool passwordVisible = false;
   bool logged = false;
 
   TextEditingController userController = TextEditingController();
@@ -36,81 +38,20 @@ class _LoginState extends State<Login> {
         elevation: 0,
         backgroundColor: Colors.transparent,
         title: Text('Anatome', style: TextStyle(color: Colors.grey[600])),
-        actionsIconTheme: IconThemeData(
-          color: Colors.grey[600]
-        ),
+        actionsIconTheme: IconThemeData(color: Colors.grey[600]),
       ),
       backgroundColor: Colors.grey[100],
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 15.0),
         child: Center(
-            child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                height: logged ? 300 : 250,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5.0)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    logged
-                        ? Container(
-                            padding: EdgeInsets.symmetric(
-                              vertical: 10,
-                              horizontal: 30,
-                            ),
-                            decoration: BoxDecoration(
-                              color: Colors.red[400],
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                            child: Text(
-                              'Usuário ou senha errados',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          )
-                        : SizedBox.shrink(),
-                    logged
-                        ? SizedBox(
-                            height: 20,
-                          )
-                        : SizedBox.shrink(),
-                    Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: Colors.pink,
-                          borderRadius: BorderRadius.circular(60),
-                        ),
-                        child: Icon(
-                          Icons.lock,
-                          color: Colors.white,
-                        )),
-                    TextField(
-                      controller: userController,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(labelText: 'Usuário'),
-                      onChanged: (text) {},
-                    ),
-                    TextField(
-                      controller: senhaController,
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                      decoration: InputDecoration(labelText: 'Senha'),
-                    ),
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    ElevatedButton(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 35),
-                        child: Text('Entrar'),
-                      ),
-                      onPressed: checkLogin,
-                    )
-                  ],
-                ))),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            height: logged ? 350 : 300,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(5.0)),
+            child: _loginFormContainer(),
+          ),
+        ),
       ),
       bottomNavigationBar: Container(
         padding: EdgeInsets.symmetric(horizontal: 15),
@@ -134,6 +75,112 @@ class _LoginState extends State<Login> {
           ],
         ),
       ),
+    );
+  }
+
+  _loginFormContainer() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.symmetric(
+        vertical: 30,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          logged
+              ? Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 30,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.red[400],
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text(
+                    'Usuário ou senha errados',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                )
+              : SizedBox.shrink(),
+          logged
+              ? SizedBox(
+                  height: 20,
+                )
+              : SizedBox.shrink(),
+          Container(
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.circular(60),
+            ),
+            child: Icon(
+              Icons.lock,
+              color: Colors.white,
+            ),
+          ),
+          TextField(
+            controller: userController,
+            keyboardType: TextInputType.text,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              labelText: 'Usuário',
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Icon(Icons.person),
+              ),
+            ),
+            onChanged: (text) {},
+          ),
+          TextField(
+            controller: senhaController,
+            keyboardType: TextInputType.text,
+            onSubmitted: (value) => checkLogin(),
+            obscureText: !passwordVisible,
+            decoration: InputDecoration(
+              labelText: 'Senha',
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Icon(Icons.lock),
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(
+                  passwordVisible ? Icons.visibility : Icons.visibility_off,
+                ),
+                onPressed: () {
+                  setState(() {
+                    passwordVisible = !passwordVisible;
+                  });
+                },
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          _submitButton()
+        ],
+      ),
+    );
+  }
+
+  Widget _submitButton() {
+    return ElevatedButton(
+      child: Container(
+        width: double.infinity,
+        height: 50,
+        child: Center(
+          child: Text(
+            'Entrar',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+      onPressed: checkLogin,
     );
   }
 }
