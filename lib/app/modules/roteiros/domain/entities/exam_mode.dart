114affff02748a8bc15f48bb9f7db51412f88fae
André Neves <andreneves3@gmail.com>
2021-04-07 00:18:37 -0300

    chore: change instructions texts

--- lib/app/modules/roteiros/domain/entities/exam_mode.dart
+++ lib/app/modules/roteiros/domain/entities/exam_mode.dart
@@ -6,32 +6,32 @@ class ExamMode {
 
   String getText() {
     if (isTheoretical && isToFind)
-      return '''Para cada conteúdo teórico informe a parte correspondente.
-
-Utilize as setas para alternar as questões, a avaliação é concluída quando todas as questões forem respondidas e clicar em “finalizar”.
-
-Gerencie bem o seu tempo e boa prova.''';
+      return '''1. Para cada conteúdo teórico informe a parte correspondente.
+2. Para alternar as questões utilize as setas ou pode clicar diretamente no ícone da questão que desejar.
+3. Após responder todas as questões, finalize sua avaliação clicando no botão “finalizar”.
+4. Pode iniciar sua avaliação clicando no botão “Iniciar exame”.
+5. Gerencie bem o seu tempo e boa prova.''';
 
     if (isTheoretical && !isToFind)
-      return '''Dada a localização de cada parte, informe o seu nome e seus respectivos conteúdos teóricos.
-
-Utilize as setas para alternar as questões, a avaliação é concluída quando todas as questões forem respondidas e clicar em “finalizar”.
-
-Gerencie bem o seu tempo e boa prova.''';
+      return '''1. Dada a localização de cada parte, informe o seu nome e seus respectivos conteúdos teóricos.
+2. Para alternar as questões utilize as setas ou pode clicar diretamente no ícone da questão que desejar.
+3. Após responder todas as questões, finalize sua avaliação clicando no botão “finalizar”.
+4. Pode iniciar sua avaliação clicando no botão “Iniciar exame”.
+5. Gerencie bem o seu tempo e boa prova.''';
 
     if (!isTheoretical && isToFind)
-      return '''Informe as partes de cada peça física do roteiro.
-
-Utilize as setas para alternar as questões, a avaliação é concluída quando todas as questões forem respondidas e clicar em “finalizar”.
-
-Gerencie bem o seu tempo e boa prova.''';
+      return '''1. Informe as partes de cada peça física do roteiro.
+2. Para alternar as questões utilize as setas ou pode clicar diretamente no ícone da questão que desejar.
+3. Após responder todas as questões, finalize sua avaliação clicando no botão “finalizar”.
+4. Pode iniciar sua avaliação clicando no botão “Iniciar exame”.
+5. Gerencie bem o seu tempo e boa prova.''';
 
     if (!isTheoretical && !isToFind)
-      return '''Para cada parte (isto é, sua localização) de cada peça física, escreva o nome da parte em questão.
-
-Utilize as setas para alternar as questões, a avaliação é concluída quando todas as questões forem respondidas e clicar em “finalizar”.
-
-Gerencie bem o seu tempo e boa prova.''';
+      return '''1. Para cada parte (isto é, sua localização) de cada peça física, escreva o nome da parte em questão.
+2. Para alternar as questões utilize as setas ou pode clicar diretamente no ícone da questão que desejar.
+3. Após responder todas as questões, finalize sua avaliação clicando no botão “finalizar”.
+4. Pode iniciar sua avaliação clicando no botão “Iniciar exame”.
+5. Gerencie bem o seu tempo e boa prova.''';
 
     return '';
   }
